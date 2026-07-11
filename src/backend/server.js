5bd2016ae59619495c36ca013ad36c57abb06fc4
Teja Battula <kbattula059@gmail.com>
2026-07-11 20:06:05 +0000

    warnings cleared

--- src/backend/server.js
+++ src/backend/server.js
@@ -25,7 +25,10 @@ mongoose.connect(process.env.MONGO_URI)
 })
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
