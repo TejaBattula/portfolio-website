2f4dc9ad4844c0abc25a1f9b10a67402e4789d5a
Teja Battula <kbattula059@gmail.com>
2026-07-11 15:49:07 +0000

    Fix Gmail SMTP configuration

--- src/backend/server.js
+++ src/backend/server.js
@@ -21,10 +21,15 @@ mongoose.connect(process.env.MONGO_URI)
 })
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 transporter.verify((error, success) => {
