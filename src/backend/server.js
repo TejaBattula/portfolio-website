23ff94461f9e5b8175fd66a425b984d72a93cde1
Teja Battula <kbattula059@gmail.com>
2026-07-11 17:01:40 +0000

    Add email error

--- src/backend/server.js
+++ src/backend/server.js
@@ -25,15 +25,16 @@ mongoose.connect(process.env.MONGO_URI)
 })
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,         // Using secure SSL port
-    secure: true,      // true for port 465
-    family: 4,         // Forces IPv4 (Crucial: Render often times out trying to use IPv6 with Gmail)
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS // Your 16-character App Password without spaces
+        pass: process.env.EMAIL_PASS
     }
 });
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
+
 transporter.verify((error, success) => {
     if (error) {
         console.log(error.message);
